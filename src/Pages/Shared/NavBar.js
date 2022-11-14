d18ee2a20008b4ba30b795f769d408c933622a1a
JfJubaer <skk5727@gmail.com>
2022-11-15 00:00:20 +0600

    7th

--- src/Pages/Shared/NavBar.js
+++ src/Pages/Shared/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -6,7 +7,7 @@ const NavBar = () => {
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="md:flex md:items-center md:gap-12">
-              <a className="block text-teal-600" href="/">
+              <Link className="block text-teal-600" href="/">
                 <span className="sr-only">Home</span>
                 <svg
                   className="h-8"
@@ -19,64 +20,64 @@ const NavBar = () => {
                     fill="currentColor"
                   />
                 </svg>
-              </a>
+              </Link>
             </div>
       
             <div className="hidden md:block">
               <nav aria-label="Site Nav">
                 <ul className="flex items-center gap-6 text-sm">
                   <li>
-                    <a
+                    <Link
                       className="text-gray-500 transition hover:text-gray-500/75"
                       href="/"
                     >
                       About
-                    </a>
+                    </Link>
                   </li>
       
                   <li>
-                    <a
+                    <Link
                       className="text-gray-500 transition hover:text-gray-500/75"
                       href="/"
                     >
                       Careers
-                    </a>
+                    </Link>
                   </li>
       
                   <li>
-                    <a
+                    <Link
                       className="text-gray-500 transition hover:text-gray-500/75"
                       href="/"
                     >
                       History
-                    </a>
+                    </Link>
                   </li>
       
                   <li>
-                    <a
+                    <Link
                       className="text-gray-500 transition hover:text-gray-500/75"
                       href="/"
                     >
                       Services
-                    </a>
+                    </Link>
                   </li>
       
                   <li>
-                    <a
+                    <Link
                       className="text-gray-500 transition hover:text-gray-500/75"
                       href="/"
                     >
                       Projects
-                    </a>
+                    </Link>
                   </li>
       
                   <li>
-                    <a
+                    <Link
                       className="text-gray-500 transition hover:text-gray-500/75"
                       href="/"
                     >
                       Blog
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </nav>
@@ -84,20 +85,20 @@ const NavBar = () => {
       
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
-                <a
+                <Link
                   className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
                   href="/"
                 >
                   Login
-                </a>
+                </Link>
       
                 <div className="hidden sm:flex">
-                  <a
+                  <Link
                     className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
                     href="/"
                   >
                     Register
-                  </a>
+                  </Link>
                 </div>
               </div>
       
